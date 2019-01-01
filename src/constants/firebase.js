53ef0556f56da383dcc82f179e0110e7f99b4a70
turnerniles <turnerniles@gmail.com>
2019-01-01 12:17:23 -0500

    Updates

--- src/constants/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyC7inoKXVvNOvLuaS6VyXgvI4Egg6kBVVQ",
-  authDomain: "minutemath-e3fe8.firebaseapp.com",
-  databaseURL: "https://minutemath-e3fe8.firebaseio.com",
-  projectId: "minutemath-e3fe8",
-  storageBucket: "minutemath-e3fe8.appspot.com",
-  messagingSenderId: "908528394289"
-};
